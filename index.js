a8e8110f56de862e66206566ed5b0ed059d8c3fa
yiwereaminu <yiwereamin@gmail.com>
2022-04-25 12:41:56 +0000

    wins and lose logic

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 console.log(randomNumber1);
 console.log(randomNumber2);
+if(randomNumber1 > randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 1 Wins"
+}
+else if(randomNumber2 > randomNumber1){
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins"
+}
+else{
+    document.getElementsByTagName("h1")[0].innerHTML = "Draw!"
+}
 
 if(randomNumber1 ===1){
     document.querySelectorAll("img")[0].setAttribute("src","images/dice1.png")
