cd72e7397980c1cafdaf61faffd407e725cf578f
yiwereaminu <yiwereamin@gmail.com>
2022-04-25 12:09:19 +0000

    added the random num denerator to the second image

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
 console.log(randomNumber1);
+console.log(randomNumber2);
 
 if(randomNumber1 ===1){
-    document.querySelector("img").setAttribute("src","images/dice1.png")
+    document.querySelectorAll("img")[0].setAttribute("src","images/dice1.png")
 }
 else if(randomNumber1 === 2){
-    document.querySelector("img").setAttribute("src","images/dice2.png")
+    document.querySelectorAll("img")[0].setAttribute("src","images/dice2.png")
 }
 else if(randomNumber1 === 3){
-    document.querySelector("img").setAttribute("src","images/dice3.png")
+    document.querySelectorAll("img")[0].setAttribute("src","images/dice3.png")
 }
 else if(randomNumber1 === 4){
-    document.querySelector("img").setAttribute("src","images/dice4.png")
+    document.querySelectorAll("img")[0].setAttribute("src","images/dice4.png")
 }
 else if(randomNumber1 === 5){
-    document.querySelector("img").setAttribute("src","images/dice5.png")
+    document.querySelectorAll("img")[0].setAttribute("src","images/dice5.png")
 }
 else{
-    document.querySelector("img").setAttribute("src","images/dice6.png")
+    document.querySelectorAll("img")[0].setAttribute("src","images/dice6.png")
 }
+
+if(randomNumber2 ===1){
+    document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png")
+}
+else if(randomNumber2 === 2){
+    document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png")
+}
+else if(randomNumber2 === 3){
+    document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png")
+}
+else if(randomNumber2 === 4){
+    document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png")
+}
+else if(randomNumber2 === 5){
+    document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png")
+}
+else{
+    document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png")
+}
+
+
