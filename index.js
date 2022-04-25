cac5a4cad285a998f96979023f5d5a3d2489d290
yiwereaminu <yiwereamin@gmail.com>
2022-04-25 07:59:40 +0000

    changed dice image for player1 using the random number

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1
-console.log(randomNumber1)
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+console.log(randomNumber1);
+
+if(randomNumber1 ===1){
+    document.querySelector("img").setAttribute("src","images/dice1.png")
+}
+else if(randomNumber1 === 2){
+    document.querySelector("img").setAttribute("src","images/dice2.png")
+}
+else if(randomNumber1 === 3){
+    document.querySelector("img").setAttribute("src","images/dice3.png")
+}
+else if(randomNumber1 === 4){
+    document.querySelector("img").setAttribute("src","images/dice4.png")
+}
+else if(randomNumber1 === 5){
+    document.querySelector("img").setAttribute("src","images/dice5.png")
+}
+else{
+    document.querySelector("img").setAttribute("src","images/dice6.png")
+}
